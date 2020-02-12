23c6e50dc842cbbf81fa6cc5df8f3950a4d80742
dabina2018 <35677021+dabina2018@users.noreply.github.com>
2020-02-12 08:56:01 -0800

    Adding Mini Bank Files

---
+++ BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp101_16
+{
+    class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal  Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance = balance + item.Amount;
+                   // balance += item.Amount; //eliminated by use of return statement
+                }
+                return balance;
+            
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            //this.balance = initialBalance;  // eliminated by making initial deposit
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+
+        }
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be positive");
+            }
+            if (Balance - amount <0)
+            {
+                throw new InvalidOperationException("Not sufficent funds for withdrawal ");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            //HEADER
+            report.AppendLine("Date\t\tAmmount\tNote");
+            foreach(var item in allTransactions)
+            {
+                //ROWS
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp101_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a 'Bank Account'  //
+            // 10digit number, name of owners, account balance, accept deposit and withdrawl
+            //initial balance should be positive(+)          //able  to show negative balance
+
+            var account = new BankAccount("Billy Bob", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            //making a withdrawal
+            account.MakeWithdrawl(120, DateTime.Now, "Hammock");
+            //Console.WriteLine(account.Balance);
+
+            account.MakeWithdrawl(50, DateTime.Now, "XBox Game");
+            //Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+            //// test error handling for (-) balance
+            //account.MakeWithdrawl(75000, DateTime.Now, "Barbie Dream House");
+            ////Console.WriteLine(account.Balance);
+
+            ////attempt to make a negative(-) deposit to test error handling
+            //account.MakeDeposit(-300, DateTime.Now, "stealing");
+
+            ////test that initial balance must be positive
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    //Console.WriteLine("Exception caught creating account with negative balance");
+            //    //Console.WriteLine(e.ToString());
+            //}
+            
+
+        }
+    }
+}
