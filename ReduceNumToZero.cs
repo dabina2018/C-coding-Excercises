c4deb84fc0f490c7521358b18f1fa43d92a7f3ec
Daveena <35677021+dabina2018@users.noreply.github.com>
2020-03-10 12:36:47 -0700

    Leet_Q5

---
+++ ReduceNumToZero.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReduceNumToZero
+{
+    class Program
+    {
+        static void Main()
+        {
+            int input = 14;
+            int input2 = 8;
+            int input3 = 123;
+
+            Console.WriteLine(NumberOfSteps(input));
+            Console.WriteLine(NumberOfSteps(input2));
+            Console.WriteLine(NumberOfSteps(input3));
+
+            Console.ReadLine();
+        }
+
+        public static int NumberOfSteps(int num)
+        {
+            int count = 0;
+            while(num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else
+                {
+                    num -= 1;
+                }
+                count++;
+
+            }
+            return count;
+        }
+
+       
+
+    }
+
+  
+}
