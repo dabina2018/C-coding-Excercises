5fa0ce5bd6e27373b5caccde1b4dd308668bc448
Daveena <35677021+dabina2018@users.noreply.github.com>
2020-03-14 12:52:58 -0700

    Update ReduceNumToZero.cs

--- ReduceNumToZero.cs
+++ ReduceNumToZero.cs
@@ -27,14 +27,8 @@ namespace ReduceNumToZero
             int count = 0;
             while(num > 0)
             {
-                if (num % 2 == 0)
-                {
-                    num /= 2;
-                }
-                else
-                {
-                    num -= 1;
-                }
+                if (num % 2 == 0) num /= 2;
+                else num -= 1;
                 count++;
 
             }
