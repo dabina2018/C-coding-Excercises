378152a5073227b23094746b70bb478d5cf6177f
dabina2018 <daveena.cooper@outlook.com>
2020-04-24 10:42:22 -0700

    added missing refrences and wrote tests for methods

---
+++ StackArray/StackArray/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackArray_Prob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackArray myArray = new StackArray();
+        }
+    }
+    public class StackArray
+    {
+        private int index = 0;
+        public int[] Array = new int[10];
+
+        public int[] Pop()
+        {
+            if (index < 0)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Array.SetValue(null, index - 1);
+                index--;
+            }
+            return Array;
+        }
+        public int[] Push(int value)
+        {
+            if (index < 0)
+            {
+                index++;
+                Console.WriteLine("Try Again...");
+            }
+            else if (index < Array.Length)
+            {
+                Array[index] = value;
+                index++;
+            }
+            else
+            {
+                Console.WriteLine("The Array is full");
+            }
+            return Array;
+        }
+        public int Peek()
+        {
+            Console.WriteLine(Array[index - 1]);
+            return Array[index];
+        }
+        public void Print()
+        {
+            Console.WriteLine($"[{string.Join(",", Array)}]");
+        }
+
+        public int[] Resize(int size)
+        {
+            if (size < Array.Length)
+            {
+                string response = "no";
+                while (response != "yes")
+                {
+                    Console.WriteLine("You have entered a size smaller than the original Array, is this correct?  (yes/no)");
+                    response = Console.ReadLine();
+                    if (response == "yes")
+                    {
+                        //TO DO: add exception handling for differently sized arrays
+                        int[] temp = new int[size];
+                        //Array.CopyTo(temp, 0);
+                        //alternate copyTo
+                        for (int i = 0; i < Array.Length - 1; i++)
+                        {
+                            temp[i] = Array[i];
+                            i++;
+                        }
+                        Array = temp;
+                    }
+                    else if (response == "no")
+                    {
+                        Console.WriteLine("Please enter a size for the new Array.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter yes or no");
+                    }
+                }
+            }
+            return Array;
+        }
+    }
+}
---
+++ StackArray/StackArrayUnitTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using StackArray_Prob;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StackArrayUnitTest
+{
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodPush()
+        {
+            int[] val = new int[10] {1,0,0,0,0,0,0,0,0,0};
+            var testInstance = new StackArray();
+            var testResult = testInstance.Push(1);
+            Assert.AreEqual(val, testResult, "The result should be an Array of size 10 with one entry");
+        }
+
+        [TestMethod]
+        public void TestMethodPop()
+        {
+            int[] val = new int[10] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            var testInstance = new StackArray();
+            var testResult = testInstance.Pop(1);
+            Assert.AreEqual(val, testResult, "The result should be an Array of size 10 with one entry");
+        }
+
+        [TestMethod]
+        public void TestMethodPeek()
+        {
+            int[] val = new int[10] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            var testInstance = new StackArray();
+            var testResult = testInstance.Peek(1);
+            Assert.AreEqual(val, testResult, "The result should be an Array of size 10 with one entry");
+        }
+
+        [TestMethod]
+        public void TestMethodResizeBigger()
+        {
+            int[] val = new int[10] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            var testInstance = new StackArray();
+            var testResult = testInstance.Resize(15);
+            Assert.AreEqual(val, testResult, "The result should be an Array of size 10 with one entry");
+        }
+        [TestMethod]
+        public void TestMethodResizeSmaller()
+        {
+            int[] val = new int[10] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            var testInstance = new StackArray();
+            var testResult = testInstance.Resize(5);
+            Assert.AreEqual(val, testResult, "The result should be an Array of size 10 with one entry");
+        }
+
+    }
+}
